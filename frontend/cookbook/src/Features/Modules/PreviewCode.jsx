1ee3cac1dc9ce088143697b8517a8fb15fe1fc7f
siva <siva.n@quadrantresource.com>
2022-01-17 12:24:04 +0530

    chnages

--- frontend/cookbook/src/Features/Modules/PreviewCode.jsx
+++ frontend/cookbook/src/Features/Modules/PreviewCode.jsx
@@ -5,21 +5,40 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { Box, Grid, Paper, TextField } from "@material-ui/core";
 import { useSelector } from "react-redux";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Container from '@material-ui/core/Container';
 
 export default function PreviewCode(props) {
   const [detaildata, setDetaildata] = useState([]);
   const id = props.InfoId;
+
+  const [isdata, setIsdata] = useState(false)
 
 
   const { menuitem } = useSelector(state => state.dashboardReducer);
   console.log(menuitem);
 
   useEffect(() => {
-     if(menuitem)
-     {
+    if (menuitem) {
 
-     
-    axios.get(`http://127.0.0.1:8000/api/detail/${menuitem||1}`).then(
+
+      axios.get(`http://127.0.0.1:8000/api/detail/${menuitem || 1}`).then(
+        (res) => {
+          console.log(res);
+          setDetaildata(res.data);
+          setIsdata(true)
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }, [menuitem]);
+  useEffect(() => {
+
+
+
+    axios.get(`http://127.0.0.1:8000/api/detail/${1}`).then(
       (res) => {
         console.log(res);
         setDetaildata(res.data);
@@ -28,83 +47,228 @@ export default function PreviewCode(props) {
         console.log(error);
       }
     );
-     }
-  }, [menuitem]);
-  useEffect(() => {
-   
 
-    
-   axios.get(`http://127.0.0.1:8000/api/detail/${1}`).then(
-     (res) => {
-       console.log(res);
-       setDetaildata(res.data);
-     },
-     (error) => {
-       console.log(error);
-     }
-   );
-    
- }, []);
- 
+  }, []);
 
 
 
-  
+  var data = null;
+  if (detaildata.length > 0) {
+    data = <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Feature Name
+          </Typography>
+          {/* <Typography component="h2"> */}
+          <div style={{ paddingLeft: 30 }}>
+            {detaildata[0].Feature_Name.split("\n").map((i, key) => {
+              return <div key={key}>{i}</div>;
+            })
+            }
+            {/* </Typography> */}
+          </div>
+
+        </Grid>
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Object Type
+          </Typography>
+          {/* <Typography component="h2"> */}
+          <div style={{ paddingLeft: 30 }}>
+            {detaildata[0].Object_Type.split("\n").map((i, key) => {
+              return <div key={key}>{i}</div>;
+            })
+            }
+            {/* </Typography> */}
+          </div>
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Source Feature Description
+          </Typography>
+          {/* <Typography component="h2"> */}
+          <div style={{ paddingLeft: 30 }}>
+            {detaildata[0].Source_FeatureDescription.split("\n").map((i, key) => {
+              return <div key={key}>{i}</div>;
+            })
+            }
+            {/* </Typography> */}
+          </div>
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Source Code
+          </Typography>
+          <div style={{ paddingLeft: 30 }}>
+            <Card style={{ paddingLeft: 30, backgroundColor: "black", color: "white" }}>
+              {/* <Typography component="h2"> */}
+              {detaildata[0].Source_Code.split("\n").map((i, key) => {
+                return <div key={key}>{i}</div>;
+              })
+              }
+            </Card>
+          </div>
+          {/* </Typography> */}
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Target Feature Description
+          </Typography>
+          {/* <Typography component="h2"> */}
+          <div style={{ paddingLeft: 30 }}>
+            {detaildata[0].Target_FeatureDescription.split("\n").map((i, key) => {
+              return <div key={key}>{i}</div>;
+            })
+            }
+            {/* </Typography> */}
+          </div>
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Target Actual Code
+          </Typography>
+          <div style={{ paddingLeft: 30 }}>
+            <Card style={{ paddingLeft: 30, backgroundColor: "black", color: "white" }}>
+              {/* <Typography component="h2"> */}
+              {detaildata[0].Target_ActualCode.split("\n").map((i, key) => {
+                return <div key={key}>{i}</div>;
+              })
+              }
+            </Card>
+          </div>
+          {/* </Typography> */}
+
+        </Grid>
+
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Target Expected Code
+          </Typography>
+          <div style={{ paddingLeft: 30 }}>
+            <Card style={{ paddingLeft: 30, backgroundColor: "black", color: "white" }}>
+              {/* <Typography component="h2"> */}
+              {detaildata[0].Target_Expected_Output.split("\n").map((i, key) => {
+                return <div key={key}>{i}</div>;
+              })
+              }
+            </Card>
+          </div>
+          {/* </Typography> */}
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Conversion Description
+          </Typography>
+          <div style={{ paddingLeft: 30 }}>
+          {/* <Card style={{ paddingLeft: 30, backgroundColor: "black", color: "white" }}> */}
+            {detaildata[0].Conversion_Description.split("\n").map((i, key) => {
+              return <div key={key}>{i}</div>;
+            })
+            }
+            {/* </Card> */}
+
+          </div>
+          {/* </Typography> */}
+
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Conversion Code
+          </Typography>
+          <div style={{ paddingLeft: 30 }}>
+            <Card style={{ paddingLeft: 30, backgroundColor: "black", color: "white" }}>
+              {/* <Typography component="h2"> */}
+              {detaildata[0].Conversion_Code.split("\n").map((i, key) => {
+                return <div key={key}>{i}</div>;
+              })
+              }
+            </Card>
+          </div>
+          {/* </Typography> */}
+
+        </Grid>
+
+
+
+
+      </Grid>
+
+
+    </>
+  }
+
 
   return (
-      <>
-    <Box py={4}>
-            <Grid container direction='row' justifyContent='center'>
-                <Grid item>
-                    <Typography variant='h6'>
-                        Detail View
-                    </Typography>
-                </Grid>
+    // <>
+    //   <Box py={4}>
+    //     <Grid container direction='row' justifyContent='center'>
+    //       <Grid item>
+    //         <Typography variant='h6'>
+    //           Detail View
+    //         </Typography>
+    //       </Grid>
 
-            </Grid>
-            </Box>
+    //     </Grid>
+    //   </Box>
 
-    <form autoComplete="off">
-      <Grid container direction="row" xs={12} spacing={4}>
-     
-        {detaildata.map((item, ind) => {
-          return (
-            <>
-              {Object.keys(item).map((list) => {
-                return (
-                  <Grid item xs={6}>
-                    <TextField
-                      id="outlined-multiline-static"
-                      label="Feature Name"
-                      multiline
-                      InputProps={{ disableUnderline: true}}
-                      label={list}
-                      rows={
-                        list ==="Source_Code" ||
-                        list ==="Target_ActualCode" ||
-                        list ==="Target_Expected_Output" ||
-                        list ==="Conversion_Code"||list==="Conversion_Description"||list==="Target_FeatureDescription"
-                        
-                          ? 10
-                          : 1
-                      }
-                    //   rows={1}
-                      value={item[list]}
-                      // defaultValue="Default Value"
-                      // onChange={handleFeaturename}
-                      variant="outlined"
-                      required
-                      fullWidth
-                      disabled
-                    />
-                  </Grid>
-                );
-              })}
-            </>
-          );
-        })}
-      </Grid>
-    </form>
-    </>
+    //   <form autoComplete="off">
+    //     <Grid container direction="row" xs={12} spacing={4}>
+
+    //       {detaildata.map((item, ind) => {
+    //         return (
+    //           <>
+    //             {Object.keys(item).map((list) => {
+    //               return (
+    //                 <Grid item xs={6}>
+    //                   <TextField
+    //                     id="outlined-multiline-static"
+    //                     label="Feature Name"
+    //                     multiline
+    //                     InputProps={{ disableUnderline: true }}
+    //                     label={list}
+    //                     rows={
+    //                       list === "Source_Code" ||
+    //                         list === "Target_ActualCode" ||
+    //                         list === "Target_Expected_Output" ||
+    //                         list === "Conversion_Code" || list === "Conversion_Description" || list === "Target_FeatureDescription"
+
+    //                         ? 10
+    //                         : 1
+    //                     }
+    //                     //   rows={1}
+    //                     value={item[list]}
+    //                     // defaultValue="Default Value"
+    //                     // onChange={handleFeaturename}
+    //                     variant="outlined"
+    //                     required
+    //                     fullWidth
+    //                     disabled
+    //                   />
+    //                 </Grid>
+    //               );
+    //             })}
+    //           </>
+    //         );
+    //       })}
+    //     </Grid>
+    //   </form>
+    // </>
+    <div>
+      {data}
+    </div>
+
   );
 }
